6003d84de809a12f9a2427d06507f48d2cc2396f
shiwenbin <1503593264@qq.com>
2021-12-07 14:01:27 +0800

    change

--- RunTest.py
+++ RunTest.py
@@ -8,13 +8,13 @@ from main import test_UI
 # 构建测试集
 def suite():
     # 创建一个测试套件
-    suite = unittest.TestSuite()
+    suites = unittest.TestSuite()
     # 将测试用例加载到测试套件中
     loader = unittest.TestLoader()  # 创建一个用例加载对象
-    suite.addTest(loader.loadTestsFromTestCase(test_UI))
-    return suite
+    suites.addTest(loader.loadTestsFromTestCase(test_UI))
+    return suites
 
 
 if __name__ == '__main__':
-    br = BeautifulReport(suite())
-    br.report(filename='testdemoreport.html', description='自动化测试报告', log_path='.', report_dir='.')
+    report = BeautifulReport(suite())
+    report.report(filename='testdemoreport.html', description='自动化测试报告', log_path='.', report_dir='.')
